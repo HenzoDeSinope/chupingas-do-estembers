494c11b2492d4c3c570e5ce827157cc7bc4aca5a
HenzoDeSinope <henzoleandro6@hotmail.com>
2024-10-31 15:46:44 -0300

    Versão Quase final do site

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ function checkPassword() {
     const correctPassword = "gian";
 
     if (inputPassword.toLowerCase() === correctPassword) {
-        document.querySelector(".header").style.display = "flex";
+        ["common", "footer", "hist"].forEach(cls => {
+            document.querySelector(`.${cls}`).style.display = "block";
+        });
         document.getElementById("passwordPrompt").style.display = "none";
     } else {
         document.getElementById("babum").innerHTML = 'Senha errada. Tente novamente.';
     }
 }
+
+
 function toggleMenu() {
     const navMenu = document.getElementById("navMenu");
+    const menuToggle = document.querySelector(".menu-toggle"); 
+
     navMenu.classList.toggle("show-menu");
+
 }
+
+window.onload = function setBg(){
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.querySelector("html").style.backgroundColor = "#" + randomColor;
+}
+
+
